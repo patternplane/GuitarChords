ce6b8c6030e766eb39d338268715155d84b8f264
patternplane <bss011202@naver.com>
2023-12-14 01:45:35 +0900

    [FEAT] 코드 구성성분 분석부 제작
    - 출력도 포함됨

---
+++ GuitarChords/src/Chord.java
@@ -0,0 +1,70 @@
+
+public class Chord {
+	private Note root;
+	private ChordType type;
+	private Note[] formingNotes;
+	
+	public Note[] chordElements() {
+		return formingNotes.clone();
+	}
+
+	static public Note[] getChordElements(Note root, ChordType type) {
+		switch (type) {
+		case M:
+			return new Note[] {
+					root,
+					NoteCalculator.ChangePitch(root, 4),
+					NoteCalculator.ChangePitch(root, 7) 
+					};
+		case m:
+			return new Note[] {
+					root,
+					NoteCalculator.ChangePitch(root, 3),
+					NoteCalculator.ChangePitch(root, 7)
+					};
+		case _7:
+			return new Note[] {
+					root,
+					NoteCalculator.ChangePitch(root, 4),
+					NoteCalculator.ChangePitch(root, 7),
+					NoteCalculator.ChangePitch(root, 10)
+					};
+		case M7:
+			return new Note[] {
+					root,
+					NoteCalculator.ChangePitch(root, 4),
+					NoteCalculator.ChangePitch(root, 7),
+					NoteCalculator.ChangePitch(root, 11)
+					};
+		case m7:
+			return new Note[] {
+					root,
+					NoteCalculator.ChangePitch(root, 3),
+					NoteCalculator.ChangePitch(root, 7),
+					NoteCalculator.ChangePitch(root, 10)
+					};
+		case mM7:
+			return new Note[] {
+					root,
+					NoteCalculator.ChangePitch(root, 3),
+					NoteCalculator.ChangePitch(root, 7),
+					NoteCalculator.ChangePitch(root, 11)
+					};
+		default:
+			return null;
+		}
+	}
+	
+	public boolean hasNote(Note note) {
+		for (Note n : formingNotes)
+			if (n == note)
+				return true;
+		return false;
+	}
+
+	public Chord(Note root, ChordType type) {
+		this.root = root;
+		this.type = type;
+		this.formingNotes = getChordElements(root, type);
+	}
+}
--- GuitarChords/src/Main.java
+++ GuitarChords/src/Main.java
@@ -5,9 +5,11 @@ public class Main {
 		GuitarNotes a = new GuitarNotes(12);
 		Note[][] b = a.getAllNotes();
 		
+		Chord c = new Chord(Note.D, ChordType.M);
+		
 		for (int i = 0; i < b.length; i ++) {
 			for (int j = 0; j < b[0].length; j++)
-				System.out.print(b[i][j] + "  ");
+				System.out.print((c.hasNote(b[i][j])?"¡Ü":"¡Û") + b[i][j] + "  ");
 			System.out.println();
 		}
 	}
